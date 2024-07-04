ee6f18843f1b94daf416469c28b39a530b040b69
vinayak2709 <vinayak09majgaonkar@gmail.com>
2024-07-04 10:17:22 +0530

    test file changed

--- test_1.py
+++ test_1.py
@@ -6,5 +6,5 @@ class TryTesting(TestCase):
     def test_always_passes(self):
         self.assertTrue(True)
 
-    def test_always_fails(self):
-        self.assertTrue(False,msg="yoyoyoyoy always fail wala to fail ho gyaa...")
+    # def test_always_fails(self):
+    #     self.assertTrue(False,msg="yoyoyoyoy always fail wala to fail ho gyaa...")
